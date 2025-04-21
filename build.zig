f9db29f010d8fe9730e4df98445a4c9803c4ae69
Carl Montanari <carl.r.montanari@gmail.com>
2025-04-21 12:27:16 -0700

    chore: fix ver extract in release, bump .pre ver

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ const libscrapli_version = std.SemanticVersion{
     .major = 0,
     .minor = 0,
     .patch = 1,
-    .pre = "alpha.1",
+    .pre = "alpha.2",
 };
 
 const targets: []const std.Target.Query = &.{
