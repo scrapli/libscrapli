d8388a58dd20a757e5e9238c65477592e5bb49df
Carl Montanari <carl.r.montanari@gmail.com>
2025-05-31 13:22:11 -0700

    chore: bump

--- build.zig
+++ build.zig
@@ -8,7 +8,7 @@ const libscrapli_version = std.SemanticVersion{
     .major = 0,
     .minor = 0,
     .patch = 1,
-    .pre = "alpha.9",
+    .pre = "alpha.10",
 };
 
 const targets: []const std.Target.Query = &.{
